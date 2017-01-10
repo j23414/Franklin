0b1f62ed358a04005dc96fe093bb1a304c0f91a2
Jennifer Chang <jenchang@iastate.edu>
2017-01-10 13:11:39 -0600

    GC content
    Compute the percentage of GC bases in a fasta file.
    Ignores case, lists the percentages by max GC content

---
+++ Computing_GC_Content/GCcontent.pl
@@ -0,0 +1,32 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $seq_name="";
+my %GC=();
+my $seq="";
+
+sub GCpercent(){
+    $GC{$seq_name}=()=$seq=~ /[GC]/gi;
+    $GC{$seq_name}=$GC{$seq_name}/length($seq);
+}
+
+while(<>){
+    chomp;
+    if(/^>(.+)/){
+	if(length($seq_name)!=0){
+	    GCpercent();
+	}
+	$seq_name=$1;
+	$seq="";
+#	print "$seq_name\n";
+    }elsif(length($seq_name)!=0){
+	$seq=$seq.$_;
+	$seq=~ s/\s//g;
+    }
+}
+GCpercent();
+for my $key (sort{$GC{$b} <=> $GC{$a} } keys %GC){
+    print "$key\t$GC{$key}\n";
+}
---
+++ Computing_GC_Content/README.md
@@ -0,0 +1,9 @@
+**Perl**
+
+Gives percentage of GC content in each sequence and sorted by max GC content
+```
+$ cat DATA/sample.txt | ./GCcontent.pl
+Rosalind_0808	0.609195402298851
+Rosalind_6404	0.5375
+Rosalind_5959	0.535714285714286
+```
